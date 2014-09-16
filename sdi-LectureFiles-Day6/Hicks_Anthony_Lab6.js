4d92b3cddae725d3d196f30df6bc586d5f4b7102
Anthony Hicks <osahon10@fullsail.edu>
2014-09-16 17:02:28 -0400

    lab6
    I hated it.

---
+++ sdi-LectureFiles-Day6/Hicks_Anthony_Lab6.js
@@ -0,0 +1,33 @@
+/*alert("JavaScript works!");
+
+ 
+
+
+myFriendsfunction = function () {
+	var myFriends = ["indian mike", "fat biff", "shaqkanistan", "mealhouse"];
+	console.log(myFriends);
+	console.log(myFriends[1],myFriends[3]);
+	  
+};
+myFriendsfunction();
+
+*/
+
+ //anthony hicks
+ //sdi lab 6
+ //september 16, 2014
+ 
+ var myNewfunction = function (){
+	
+	var myLadies = ["cherokee", "roxy reynolds", "nyomi banxxx", "jadafire"];
+	var myClubz = ["skylark", "macombos", "king of diamonds", "magic city"];
+	var loopCounter;
+	// for loop break down... for(new defined variable can be whatever=u) = 0 or wherever u want to start. then u day what is thsi new variable u = platinum chains.
+	for (var i = 0; i < myLadies.length; i++){
+		console.log("I just saw "  + myLadies[i] +  "  working  at  "  + myClubz[i] +   "  last time i was in that city");
+		
+
+	};
+};
+myNewfunction();
+
